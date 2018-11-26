31c0cbb16df8186bb9edf90407ed7b8e0e4962cf
torkhau <pinhead_@tut.by>
2018-11-26 19:38:35 +0300

    Релиз

---
+++ Start/Lesson3/Task3_Real/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3_Real
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float pi = 3.14f;
+            Console.Write("Введите радиус круга: ");
+            int r = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Площадь круга с радиусом {0}, равна - {1}", r, (float)(pi*Math.Pow(r, 2)));
+            Console.ReadKey();
+        }
+    }
+}
