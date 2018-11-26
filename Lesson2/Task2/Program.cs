70d5258c49becd962413ea3e597f9035095da0f1
torkhau <pinhead_@tut.by>
2018-11-26 16:53:22 +0300

    Добавление Основного задания №2 и №3

---
+++ Lesson2/Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double Pi = 3.141592653;
+            decimal e = 2.7182818284590452M;
+            Console.WriteLine(Pi);
+            Console.WriteLine(e);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson2/Task3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s1 = "\nмоя строка 1";
+            string s2 = "\tмоя строка 2";
+            string s3 = "\aмоя строка 3";
+            Console.Write(s3);
+            Console.ReadKey();
+        }
+    }
+}
