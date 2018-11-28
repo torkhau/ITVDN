7b81f42a27e867897e0c55b1a6d2621ae8938c1a
torkhau <pinhead_@tut.by>
2018-11-28 11:35:22 +0300

    Добавил задачи 4 и 5

---
+++ C#/Start/Lesson3/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const float pi = 3.14f;
+            Console.Write("Введите радиус цилиндра: ");
+            int R = Int32.Parse(Console.ReadLine());
+            Console.Write("Введите высоту цилиндра: ");
+            int h = Int32.Parse(Console.ReadLine());
+
+            Console.WriteLine("Объём цилиндра с радиусом {0} и высотой {1}, равен - {2}", R, h, (float)(pi * Math.Pow(R, 2) * h));
+            Console.WriteLine("Площадь поверхности цилиндра с радиусом {0} и высотой {1}, равена - {2}", R, h, (float)(2 * pi * R * (R + h)));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ C#/Start/Lesson3/Task5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //bool uberflu? = false; Ошибка, такую переменную нельзя использовать
+            bool _Identifier = false;
+            bool \u006fIdentifier = false;
+            //bool &myVar = false; Ошибка, такую переменную нельзя использовать
+            bool myVariab1le = false;
+        }
+    }
+}
